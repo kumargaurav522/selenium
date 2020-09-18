b404f660ae39c75d1a18e9462d497523046ec08b
Geeta Suryawanshi <geeta.suryawanshi@exusys.com>
2020-09-18 11:09:06 +0530

    first commit 5

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
+driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
 driver.set_page_load_timeout("10")
 driver.get("https://www.google.com/")
 driver.maximize_window()
