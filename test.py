2cf4c5361f6db78cc2b8dc7beb69eb61012ff69a
Geeta Suryawanshi <geeta.suryawanshi@exusys.com>
2020-09-18 11:05:57 +0530

    first commit 4

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
 driver.set_page_load_timeout("10")
