52672d18d6866ecf426913576379f0af2bd88fe1
Geeta Suryawanshi <geeta.suryawanshi@exusys.com>
2020-09-19 15:06:00 +0530

    first commit 11

--- test.py
+++ test.py
@@ -3,15 +3,18 @@ import time
 from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# options = webdriver.ChromeOptions()
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-chrome_options.add_argument('--disable-infobars')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument("--user-data-dir=/home/gaurav/.config/google-chrome/Default")
+# chrome_options.add_argument('--disable-infobars')
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument("--user-data-dir=/home/gaurav/.config/google-chrome/Default")
 # chrome_options.add_argument('--disable-dev-shm-usage')
 # chrome_options.add_argument('--remote-debugging-port=9222')
 
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
+# driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
+driver = webdriver.Remote("http://192.168.1.2:4444/wd/hub", DesiredCapabilities.CHROME)
 driver.set_page_load_timeout("10")
 driver.get("https://www.google.com/")
 driver.maximize_window()
