9183a7c46e4d25cd4fda8db720f930fc2e57eb5a
Geeta Suryawanshi <geeta.suryawanshi@exusys.com>
2020-09-22 14:54:34 +0530

    first commit 15

--- test2.py
+++ test2.py
@@ -1,21 +1,22 @@
 import time
 import unittest
-import os
-
+import os    
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
 class ITTroubleshooterSearchTest(unittest.TestCase):
 
+    # def setUp(self):
+    #     caps = {'browserName': os.getenv('firefox', 'firefox')}
+    #     self.browser = webdriver.Remote(
+    #         command_executor='http://192.168.1.2:4444/wd/hub',
+    #         desired_capabilities=caps
+    #     )
     def setUp(self):
-        caps = {'browserName': os.getenv('firefox', 'firefox')}
-        self.browser = webdriver.Remote(
-            command_executor='http://192.168.1.2:4444/wd/hub',
-            desired_capabilities=caps
-        )
+        self.browser = webdriver.Chrome()
 
-    def search_for_ittroubleshooter(self):
+    def test_ITTroubleshooter_search_for(self):
         browser = self.browser
         browser.get('https://www.google.com/')
         search_box = browser.find_element_by_name('q')
