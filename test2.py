5cadc15a5ffc18c1811dca4b2c11f7556c18f936
Geeta Suryawanshi <geeta.suryawanshi@exusys.com>
2020-09-22 14:07:32 +0530

    first commit 16

--- test2.py
+++ test2.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class HackerNewsSearchTest(unittest.TestCase):
+class ITTroubleshooterSearchTest(unittest.TestCase):
 
     def setUp(self):
         caps = {'browserName': os.getenv('firefox', 'firefox')}
@@ -14,10 +14,8 @@ class HackerNewsSearchTest(unittest.TestCase):
             command_executor='http://192.168.1.2:4444/wd/hub',
             desired_capabilities=caps
         )
-    # def setUp(self):
-    #     self.browser = webdriver.Chrome()
 
-    def test_hackernews_search_for_testdrivenio(self):
+    def search_for_ittroubleshooter(self):
         browser = self.browser
         browser.get('https://www.google.com/')
         search_box = browser.find_element_by_name('q')
@@ -25,34 +23,6 @@ class HackerNewsSearchTest(unittest.TestCase):
         search_box.send_keys(Keys.RETURN)
         browser.get('https://ittroubleshooter.in/')
         time.sleep(3)  # simulate long running test
-        # self.assertIn('ittroubleshooter', browser.page_source)
-
-    # def test_hackernews_search_for_selenium(self):
-    #     browser = self.browser
-    #     browser.get('https://news.ycombinator.com')
-    #     search_box = browser.find_element_by_name('q')
-    #     search_box.send_keys('selenium')
-    #     search_box.send_keys(Keys.RETURN)
-    #     time.sleep(3)  # simulate long running test
-    #     self.assertIn('selenium', browser.page_source)
-
-    # def test_hackernews_search_for_testdriven(self):
-    #     browser = self.browser
-    #     browser.get('https://news.ycombinator.com')
-    #     search_box = browser.find_element_by_name('q')
-    #     search_box.send_keys('testdriven')
-    #     search_box.send_keys(Keys.RETURN)
-    #     time.sleep(3)  # simulate long running test
-    #     self.assertIn('testdriven', browser.page_source)
-
-    # def test_hackernews_search_with_no_results(self):
-    #     browser = self.browser
-    #     browser.get('https://news.ycombinator.com')
-    #     search_box = browser.find_element_by_name('q')
-    #     search_box.send_keys('?*^^%')
-    #     search_box.send_keys(Keys.RETURN)
-    #     time.sleep(3)  # simulate long running test
-    #     self.assertNotIn('<em>', browser.page_source)
 
     def tearDown(self):
         self.browser.quit()  # quit vs close?
